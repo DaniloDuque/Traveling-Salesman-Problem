c3a8f496adbb18755fc84f72fd89f23201d828a2
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-12-28 17:16:33 -0600

    Create pair.java

---
+++ code/pair.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class pair<t1, t2>{
+
+    public t1 first;
+    public t2 second;
+
+    public pair(t1 x, t2 y){
+        first = x;
+        second = y;
+    }
+    
+
+}
