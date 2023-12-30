911d2fdc3c5e54ac04954116adc8b9a2b432a339
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-12-30 13:54:53 -0600

    Update Graph.java

--- code/Graph.java
+++ code/Graph.java
@@ -1,73 +1,27 @@
 import java.util.*;
 
+class Graph{
 
-
-class edge{
-
-    public int next;
-    public int cost;
-
-    public edge(int destination, int cost){
-
-        this.next = destination;
-        this.cost = cost;
-
-    }
-
-
-}
-
-
-class node{
-
-    private int id;
-    private List<edge> adjacent;
-
-    public node(int id){
-
-        this.id = id;
-        this.adjacent = new ArrayList<>();
-
-    }
-
-    public void addAdjacent(int neigh, int cost){
-        adjacent.add(new edge(neigh, cost));
-    }
-
-    public List<edge> getAdjacent(){
-        return adjacent;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-}
-
-
-
-public class Graph{
-
-    private Map<Integer, node> nodes;
+    private int[][] adjacencyMatrix;
     private int size;
+    private final int INF = 1<<20;
 
-    public Graph(int size){
+    public Graph(int size) {
         this.size = size;
-        nodes = new HashMap<>();
-        for(int i = 1; i<=size; i++) nodes.put(i, new node(i));
+        this.adjacencyMatrix = new int[size][size];
+        for(int i = 0; i<size; i++)
+            for(int j = 0; j<size; j++)
+                adjacencyMatrix[i][j] = INF;
     }
 
-    public void addEdge(int sourceId, int destinationId, int cost){
-        node source = nodes.get(sourceId);
-        node destination = nodes.get(destinationId);
-        source.addAdjacent(destinationId, cost);
-        destination.addAdjacent(sourceId, cost);
+    public void addEdge(int sourceId, int destinationId, int cost) {
+        adjacencyMatrix[sourceId][destinationId] = cost;
+        adjacencyMatrix[destinationId][sourceId] = cost;
     }
 
-
-    public List<edge> getAdjacent(int nodeId){
-        return nodes.get(nodeId).getAdjacent();
+    public int getCost(int sourceId, int destinationId) {
+        return adjacencyMatrix[sourceId][destinationId];
     }
-
+        
 
 }
