dd1255470fff057a22cb9c32a852c7410bd940c6
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-01-02 13:25:32 -0600

    Update BackTSP.java

--- code/BackTSP.java
+++ code/BackTSP.java
@@ -6,11 +6,14 @@ public class BackTSP{
     private int V;
     private Graph graph;
     private final int INF = 1<<20;
+    private int START;
+    private int END_STATE;
 
     public BackTSP(Graph graph, int size){
 
         this.graph = graph;
         V = size;
+        END_STATE = (1<<V)-1;
 
     }
 
@@ -32,14 +35,11 @@ public class BackTSP{
     }
 
 
-    private pair<Integer, List<Integer>> findMin(int start, int curr, int msk, List<Integer> path, int cost, int cont){
-
-        if(curr == start) cont++;
-        else msk = SET(msk, curr);
+    private pair<Integer, List<Integer>> findMin(int curr, int msk, ArrayList<Integer> path, int cost){
 
         path.add(curr);
 
-        if(curr == start && cont == 2 && path.size() > V) return new pair<>(cost, path);
+        if(msk == END_STATE) return new pair<>(graph.getCost(curr, START) + cost, path);
 
         pair<Integer, List<Integer>> rslt = new pair<>(INF, new ArrayList<>());
 
@@ -47,7 +47,7 @@ public class BackTSP{
 
             if(graph.getCost(curr, i) != INF && !TEST(msk, i)) 
 
-                rslt = min(rslt, findMin(start, i, msk, new ArrayList<>(path), cost + graph.getCost(curr, i), cont));
+                rslt = min(rslt, findMin(i, SET(msk, i), new ArrayList<>(path), cost + graph.getCost(curr, i)));
 
         return rslt;
 
@@ -57,8 +57,12 @@ public class BackTSP{
     private pair<Integer, List<Integer>> solveTSP(){
 
         pair<Integer, List<Integer>> rslt = new pair<>(INF, new ArrayList<>());
-        for(int i = 0; i<V; i++) rslt = min(rslt, findMin(i, i, 0, new ArrayList<>(), 0, 0));
-        return rslt;
+        for(int i = 0; i<V; i++){
+            
+            START = i;
+            rslt = min(rslt, findMin(i, (1<<i), new ArrayList<>(), 0));
+        
+        }return rslt;
 
     }
 
@@ -67,12 +71,11 @@ public class BackTSP{
 
         pair<Integer, List<Integer>> rslt = solveTSP();
         System.out.println(rslt.first);
+        System.out.print(rslt.second.get(rslt.second.size()-1) + 1 + " ");
         for(Integer i: rslt.second) System.out.print(i+1 + " ");
         System.out.println();
 
     }
-
-
 
 
 }
